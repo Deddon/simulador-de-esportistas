5fd64ec9701112dc461585e53508eb94dde6c56f
Deddon <goncalves.c2004@gmail.com>
2026-01-18 16:29:16 -0300

    Add sportsman class and constants singleton.

---
+++ features/sportsman/sportsman.gd
@@ -0,0 +1,31 @@
+class_name Sportsman
+extends Resource
+
+
+const DEFAULT_HEIGHT_CM := 180
+const DEFAULT_AGE_YEARS := 24
+
+const DEFAULT_TRAINING_WEEK_FREQUENCY := 5
+const DEFAULT_TRAINING_DURATION_MIN := 40
+const DEFAULT_AVERAGE_TRAINING_MINUTES_DAY := (DEFAULT_TRAINING_WEEK_FREQUENCY * DEFAULT_TRAINING_DURATION_MIN) / 7.0
+
+
+@export var name: String
+@export var weight_kg: float = 60.0
+@export var current_sport: Constants.SportsmanAvailableSports = Constants.SportsmanAvailableSports.NULL
+
+
+func calculate_energy() -> float:
+	var MBR: float = harris_benedict()
+	var MET: float = met_expenditure()
+	return MBR + MET
+
+
+func harris_benedict() -> float:
+	return 66.47 + (13.75 * weight_kg) + (5.003 * DEFAULT_HEIGHT_CM) - (6.775 * DEFAULT_AGE_YEARS)
+
+
+func met_expenditure() -> float:
+	var calories_per_minute: float
+	calories_per_minute = (Constants.AVAILABLE_SPORTS_MET[current_sport] * 3.5 * weight_kg) / 200
+	return calories_per_minute * DEFAULT_AVERAGE_TRAINING_MINUTES_DAY
---
+++ shared/autoloads/constants.gd
@@ -0,0 +1,17 @@
+extends Node
+
+
+enum SportsmanAvailableSports {
+	NULL,
+	OLYMPIC_WEIGHT_LIFTER,
+	LONG_RUNNER,
+	FOUR_HUNDRED_METERS_RUNNER
+}
+
+
+const AVAILABLE_SPORTS_MET = [
+	0.0, # Null
+	6.0, # LPO - Resistance (weight lifting - free weight, nautilus or universal-type), power lifting or body building, vigorous effort (Taylor Code 210)
+	11.0, # Long Run - Running, 7 mph (8.5 min/mile)
+	10.0 # Speed Run - Running, on a track, team practic
+]
