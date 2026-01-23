27bded5913c3f1f6ea4abf64bcfaf8f667be9122
Deddon <goncalves.c2004@gmail.com>
2026-01-23 03:09:50 -0300

    Add scenes for instantiation on main menu workspace tabs.

--- features/menus/main_menu/workspace/workspace.gd
+++ features/menus/main_menu/workspace/workspace.gd
@@ -5,23 +5,49 @@ extends Control
 @export var main_container: Container
 @export var tabs_container: Container
 
+var instantiated_tabs_scene: Dictionary[int, Control] = {}
+
 
 func _ready() -> void:
 	_create_tabs_button_group()
+	_show_visao_geral_at_start()
 
 
 func _create_tabs_button_group() -> void:
 	var tabs_button_group := ButtonGroup.new()
+	var tabs_list = tabs_container.get_children() as Array[MainMenuWorkspaceTab]
 	
-	for tab: MainMenuWorkspaceTab in tabs_container.get_children():
-		tab.mouse_detector_button.button_group = tabs_button_group
-		tab.tab_selected.connect(_handle_tab_selection.bind(tab))
-		tab.tab_desselected.connect(_handle_tab_desselection.bind(tab))
+	for tab_index: int in tabs_list.size():
+		var current_tab: MainMenuWorkspaceTab = tabs_list[tab_index]
+		
+		current_tab.mouse_detector_button.button_group = tabs_button_group
+		current_tab.tab_selected.connect(_handle_tab_selection.bind(tab_index))
+		current_tab.tab_desselected.connect(_handle_tab_desselection.bind(tab_index))
 
 
-func _handle_tab_selection(_tab_packed_scene: PackedScene, _selected_tab: MainMenuWorkspaceTab) -> void:
-	pass
+func _show_visao_geral_at_start() -> void:
+	instantiated_tabs_scene[0] = %VisaoGeral.current_packed_scene.instantiate()
+	main_container.add_child(instantiated_tabs_scene[0])
+	instantiated_tabs_scene[0].show()
 
 
-func _handle_tab_desselection(_selected_tab: MainMenuWorkspaceTab) -> void:
-	pass
+func _handle_tab_selection(tab_packed_scene: PackedScene, selected_tab: int) -> void:
+	if not tab_packed_scene:
+		return
+	
+	for node: Control in main_container.get_children():
+		if not (node is Container):
+			node.hide()
+	
+	if not instantiated_tabs_scene.has(selected_tab):
+		var tab_scene: Control = tab_packed_scene.instantiate()
+		instantiated_tabs_scene[selected_tab] = tab_scene
+		
+		main_container.add_child(instantiated_tabs_scene[selected_tab])
+	
+	instantiated_tabs_scene[selected_tab].show()
+
+
+func _handle_tab_desselection(selected_tab: int) -> void:
+	instantiated_tabs_scene[selected_tab].hide()
+	instantiated_tabs_scene[0].show()
